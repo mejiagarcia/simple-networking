022414ace68f0fb602e9f361d6b0bf57529741ff
Carlos Mejía <luiscarlos083@hotmail.com>
2019-12-02 20:26:23 -0500

    Add initial project structure.

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Luis Carlos Mejia Garcia on 2/12/19.
+//  Copyright © 2019 Luis Carlos Mejia Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleNetworking/SimpleNetworking/SimpleNetworking.h
@@ -0,0 +1,19 @@
+//
+//  SimpleNetworking.h
+//  SimpleNetworking
+//
+//  Created by Luis Carlos Mejia Garcia on 2/12/19.
+//  Copyright © 2019 Luis Carlos Mejia Garcia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleNetworking.
+FOUNDATION_EXPORT double SimpleNetworkingVersionNumber;
+
+//! Project version string for SimpleNetworking.
+FOUNDATION_EXPORT const unsigned char SimpleNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleNetworking/PublicHeader.h>
+
+
